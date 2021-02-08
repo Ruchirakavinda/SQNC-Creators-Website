cd4113faa71dbc085ca523fd4ec4772a57c04452
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-02-08 09:27:49 +0530

    form backend done

---
+++ php/contactt.php
@@ -0,0 +1,23 @@
+
+<?php
+
+if(isset($_POST["sent"])){
+
+     $uname = $_POST["namee"];
+     $email = $_POST["email"];
+     $message = $_POST["msg"];
+
+
+     $to = "sqnccreators@gmail.com";
+
+     $subject = "From Website Contact Form";
+
+     $nmsg = $uname . ": " . $message;
+     
+     if(mail($to,$subject,$nmsg,$email)){
+        echo "<script> alert('Your Message has been sent, Thank You..!'); window.location.href='index.php'; </script>";
+     }
+ }
+
+
+?>
